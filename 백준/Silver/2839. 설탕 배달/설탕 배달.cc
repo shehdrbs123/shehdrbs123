6495f6098d07039f726ba25910c6ad6728c4d398
DongKyun Noh <shehdrbs123@naver.com>
2025-05-30 15:56:01 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,38 +1,29 @@
-//#include <bits/stdc++.h>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);
-    cin.tie(NULL);
+    cin.tie(0);
     
-    int input;
+    int weight, resultBack=10000;
+    cin >> weight;
     
-    cin >> input;
-
-
-    for(int i = input/5;i>=0;--i)
+    for(int i = weight/5;i>=0;--i)
     {
-        int rest = input - (5*i);
-        
-        int packed = i;
-        if(rest != 0)
-        {
-            int threeRest = rest % 3;
-            if(threeRest > 0)
-                continue;
-            
-            cout << i + (int) rest/3;
-            return 0;
-        }
-        else
-        {
-            cout << i;
-            return 0 ;
-        }
+        int rest = weight - (5*i);
 
+        if(rest%3 == 0)
+        {
+            int curBack = rest/3 + i;
+            if(resultBack > curBack)
+                resultBack = curBack;
+        }
     }
-
-    cout << -1;    
+    
+    if(resultBack == 10000)
+        resultBack = -1;
+        
+    cout << resultBack;
 }
