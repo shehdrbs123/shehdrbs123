8253ff7775df9a8ca0bb42bb22d27d41fc8830bd
DongKyun Noh <shehdrbs123@naver.com>
2025-06-04 18:12:20 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <string>
+#include <cmath>
 using namespace std;
 
 int dp[1001][1001];
@@ -8,43 +9,32 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    string input1;
-    string input2;
-    
-    cin>>input1;
-    cin>>input2;
-    
-    int aSize = input1.size();
-    int bSize = input2.size();
-    
-    if(aSize > bSize)
-    {
-        int tmp = aSize;
-        aSize = bSize;
-        bSize = tmp;
+    string a,b;
+    cin >> a;
+    cin >> b;
 
-        string tmpInput = input1;
-        input1 = input2;
-        input2 = tmpInput;
-    }
-        
+    int aSize = a.size();
+    int bSize = b.size();
+    int result = 0;
     
-    for(int i=1;i<=aSize;++i)
+    for(int i=0;i<aSize;++i)
     {
-        for(int j=1;j<=bSize;++j)
+        for(int j=0;j<bSize;++j)
         {
-            char a = input1[i-1];
-            char b = input2[j-1];
-            if(a == b)
+            int dpAidx = i+1;
+            int dpBidx = j+1;
+            
+            if(a[i] == b[j])
             {
-                dp[i][j] = max(dp[i][j-1],dp[i-1][j-1]+1);
-            }
-            else
+                dp[dpAidx][dpBidx] = dp[dpAidx-1][dpBidx-1] + 1;
+            }else
             {
-                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                dp[dpAidx][dpBidx] = max(dp[dpAidx-1][dpBidx],dp[dpAidx][dpBidx-1]);
             }
+            
+            result = max(result,dp[dpAidx][dpBidx]);
         }
     }
     
-    cout << dp[aSize][bSize];
+    cout << result;
 }
