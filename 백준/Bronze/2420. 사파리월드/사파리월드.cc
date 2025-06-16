f77a3d9e96feba740ec00a1021cb721f6e319835
DongKyun Noh <shehdrbs123@naver.com>
2025-06-16 15:14:42 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    long long N,M;
+    
+    cin >> N >> M;
+    
+    cout << abs(N-M);
+    
+}
